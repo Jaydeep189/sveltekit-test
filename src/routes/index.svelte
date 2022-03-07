db045bed1e0dd91b5808fd5c51f1fe73cb81d764
Jaydeep <sjaydeep189@gmail.com>
2022-03-07 20:20:31 +0530

    First Commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,30 @@
 <script context="module">
+import ApplyNow from "$lib/components/ApplyNow.svelte";
+import Cources from "$lib/components/Cources.svelte";
+import Facts from "$lib/components/Facts.svelte";
+import Main from "$lib/components/Main.svelte";
+import NewMeet from "$lib/components/NewMeet.svelte";
+import Services from "$lib/components/Services.svelte";
+
 	export const prerender = true;
 </script>
 
-<script>
-	import Counter from '$lib/Counter.svelte';
-</script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<main>
+	<Main />
+	<!-- ***** Main Banner Area End ***** -->
+  
+	<Services/>
+  
+	<NewMeet />
+  
+	<ApplyNow />
+  
+	<Cources />
+  
+	<Facts />
+</main>
